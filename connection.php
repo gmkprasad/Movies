70f669ddbf369fa3006b1609cc97b07e576e36c5
G.M.K.Prasad <prasadchoco@gmail.com>
2018-03-05 20:59:44 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,10 +3,10 @@
  
 function Connect()
 {
- $dbhost = "sql306.byethost9.com";
- $dbuser = "b9_21646915";
- $dbpass = "bobby@123";
- $dbname = "b9_21646915_movies";
+ $dbhost = "";
+ $dbuser = "";
+ $dbpass = "";
+ $dbname = "";
  
  // Create connection
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
